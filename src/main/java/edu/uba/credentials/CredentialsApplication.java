c2ce76ff2c93ebe653381f48d1e32dd7129df381
damianVarela <damian@skedgo.com>
2024-11-24 17:51:33 -0300

    first commit

---
+++ src/main/java/edu/uba/credentials/CredentialsApplication.java
@@ -0,0 +1,13 @@
+package edu.uba.credentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredentialsApplication.class, args);
+    }
+
+}
